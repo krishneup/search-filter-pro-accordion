e72c149a311fc03459d16edde0ee55a0f0e4d2d1
Krishna Neupane <60743337+krishneup@users.noreply.github.com>
2022-03-02 17:29:56 +0545

    Update search-filter-pro-accordion.js
    Minor fixes for hierarchial checkbox options

--- search-filter-pro-accordion.js
+++ search-filter-pro-accordion.js
@@ -2,18 +2,18 @@
 function accordionLoader() {
 
     //this will wrap in checkbox field only, you can add for other fields if needed
-    jQuery("li[data-sf-field-input-type^='checkbox'] h4").wrap("<a class='accordion-title' href='#'></a>");
+    jQuery(".searchandfilter>ul>li[data-sf-field-input-type^='checkbox'] h4").wrap("<a class='accordion-title' href='#'></a>");
 
     // use this if you want to add icon after widget title, use custom css for styling
-    jQuery("li[data-sf-field-input-type^='checkbox'] h4").append('  <i class="fa fa-chevron-circle-down icon-class" aria-hidden="true"></i>');
+    jQuery(".searchandfilter>ul>li[data-sf-field-input-type^='checkbox'] h4").append('  <i class="fa fa-chevron-circle-down icon-class" aria-hidden="true"></i>');
 
     // add class to input field 'ul' 
-    jQuery("li[data-sf-field-input-type^='checkbox'] ul").addClass("accordion-list");
+    jQuery(".searchandfilter>ul>li[data-sf-field-input-type^='checkbox']>ul").addClass("accordion-list");
 
     var allAccordions = jQuery('.accordion-list').hide();
 
     //when element is clicked, toggle it for checkbox
-    jQuery("li[data-sf-field-input-type^='checkbox'] a").click(function () {
+    jQuery(".searchandfilter>ul>li[data-sf-field-input-type^='checkbox']>a").click(function () {
         event.preventDefault();
         //allAccordions.slideUp();
         jQuery(this).next().slideToggle();
